f0cc8e50d1b5e0532e61995f45ea12b383952e8e
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-11-14 16:52:38 +0900

    Undo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:edit, :update, :destroy]
   devise_for :users
   root to: "messages#index"
+  resources :users, only: [:edit, :update, :destroy] 
 end
