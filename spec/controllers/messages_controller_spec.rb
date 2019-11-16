4466f76cb4938ec788ba707a9be3ef7517beda1b
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-11-16 20:40:23 +0900

    test

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe MessagesController do
+
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+    context 'log in' do
+
+    end
+
+    context 'not log in' do
+
+    end
+end
