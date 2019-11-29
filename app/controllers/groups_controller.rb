78ad9b6a17d279f6f372cff444bc3d7c3c66a53e
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-11-29 17:10:21 +0900

    side_varに関連する記述を元に戻した
    トップページが真っ白になってしまっていたため。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,10 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
+  def index
+    @group = Group.all
+    @members = @group.name
+  end
   
   def new
     @group = Group.new
