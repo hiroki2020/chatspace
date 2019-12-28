3da69145c8713e84ffb48cfd83604c225556477e
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-28 18:46:56 +0900

    rootパス　記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'groups#index'
+  root 'groups#index'
   resources :users, only: [:index, :edit, :update, :destroy]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create] 
