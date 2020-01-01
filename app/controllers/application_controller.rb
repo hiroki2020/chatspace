2b14c26ae42b4f31ccd4a06205e86b8dcf4097d3
hiroki2020 <n64.shibata.hiroki@gmail.com>
2020-01-01 23:48:53 +0900

    Basic認証の記述を消してみた
    本番環境でエラーが起こったため

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth, if: :production?
+  
   protect_from_forgery with: :exception
   
   before_action :authenticate_user!
@@ -11,11 +11,7 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-  #   end
-  # end 
+  
 
   protected
 
